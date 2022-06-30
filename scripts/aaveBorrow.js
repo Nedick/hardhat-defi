db7591bd64085fafb07602451bf7d4c7b63de190
Nedelin Slavov <nedelin.slavov@gmail.com>
2022-06-30 17:18:50 +0300

    Added borrow script for interaction with AAVE

---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,123 @@
+const { getWeth, AMOUNT } = require("../scripts/getWeth.js")
+const { getNamedAccounts, ethers, network } = require("hardhat")
+const { networkConfig } = require("../helper-hardhat-config")
+
+async function main() {
+  //the protocol treats everything as and ERC20 token
+  await getWeth()
+  const { deployer } = await getNamedAccounts()
+  const lendingPool = await getLendingPool(deployer)
+  console.log(`Lending pool address: ${lendingPool.address}`)
+
+  // deposit!
+  const wethTokenAddress = networkConfig[network.config.chainId].wethToken
+  // approve!!
+  await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
+  console.log("Depositing WETH...")
+  await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
+  console.log("Deposit complete!")
+  let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(
+    lendingPool,
+    deployer
+  )
+  const daiPrice = await getDaiPrice()
+  const amountDaiToBorrow =
+    availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber())
+  console.log(`You can borrow ${amountDaiToBorrow} DAI`)
+  const amountDaiToBorrowWei = ethers.utils.parseEther(
+    amountDaiToBorrow.toString()
+  )
+  // borrow!
+  // how much we have borrowed, how much we have in collateral, how much we can borrow
+  await borrowDai(
+    networkConfig[network.config.chainId].daiToken,
+    lendingPool,
+    amountDaiToBorrowWei,
+    deployer
+  )
+  await getBorrowUserData(lendingPool, deployer)
+  await repay(
+    amountDaiToBorrowWei,
+    networkConfig[network.config.chainId].daiToken,
+    lendingPool,
+    deployer
+  )
+  await getBorrowUserData(lendingPool, deployer)
+}
+
+async function repay(amount, daiAddress, lendingPool, account) {
+  await approveErc20(daiAddress, lendingPool.address, amount, account)
+  const repayTx = await lendingPool.repay(daiAddress, amount, 1, account)
+  await repayTx.wait(1)
+  console.log("Repayment complete!")
+}
+
+async function borrowDai(
+  daiAddress,
+  lendingPool,
+  amountDaiToBorrowWei,
+  account
+) {
+  const borrwoTx = await lendingPool.borrow(
+    daiAddress,
+    amountDaiToBorrowWei,
+    1,
+    0,
+    account
+  )
+  await borrwoTx.wait(1)
+  console.log(`You have borrowed ${amountDaiToBorrowWei} worth of DAI`)
+}
+
+async function getDaiPrice() {
+  const daiEthPriceFeed = await ethers.getContractAt(
+    "AggregatorV3Interface",
+    networkConfig[network.config.chainId].daiEthPriceFeed
+  )
+  const price = (await daiEthPriceFeed.latestRoundData())[1]
+  console.log(`The DAI/ETH price is ${price.toString()}`)
+  return price
+}
+
+async function getBorrowUserData(lendingPool, account) {
+  const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+    await lendingPool.getUserAccountData(account)
+  console.log(`You have ${totalCollateralETH} worth of ETH deposited.`)
+  console.log(`You have ${totalDebtETH} worth of ETH borrowed.`)
+  console.log(`You have borrow ${availableBorrowsETH} worth of ETH.`)
+  return { availableBorrowsETH, totalDebtETH }
+}
+
+async function getLendingPool(account) {
+  const lendingPoolAddressesProvider = await ethers.getContractAt(
+    "ILendingPoolAddressesProvider",
+    networkConfig[network.config.chainId].lendingPoolAddressesProvider,
+    account
+  )
+  const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
+  const lendingPool = await ethers.getContractAt(
+    "ILendingPool",
+    lendingPoolAddress,
+    account
+  )
+  return lendingPool
+}
+
+async function approveErc20(
+  erc20Address,
+  spenderAddress,
+  amountToSpend,
+  account
+) {
+  const erc20Token = await ethers.getContractAt("IERC20", erc20Address, account)
+  const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+  await tx.wait(1)
+  console.log("Token Approved!")
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
